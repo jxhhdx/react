feb3a68fb8dce4cdeb65d8d9b569d6acbb00ddc3
gxuud <igaoxian@foxmail.com>
2024-01-15 20:48:40 +0800

    feat: update

---
+++ build/facebook-www/ReactSharedSubset-prod.modern.js
@@ -0,0 +1,492 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @noflow
+ * @nolint
+ * @preventMunge
+ * @preserve-invariant-messages
+ */
+
+"use strict";
+var assign = Object.assign,
+  enableTransitionTracing =
+    require("ReactFeatureFlags").enableTransitionTracing,
+  ReactCurrentCache = { current: null },
+  ReactCurrentDispatcher = { current: null },
+  ReactCurrentOwner$1 = { current: null },
+  ReactSharedInternals = {
+    ReactCurrentDispatcher: ReactCurrentDispatcher,
+    ReactCurrentOwner: ReactCurrentOwner$1
+  },
+  ReactServerSharedInternals = { ReactCurrentCache: ReactCurrentCache },
+  REACT_ELEMENT_TYPE = Symbol.for("react.element"),
+  REACT_PORTAL_TYPE = Symbol.for("react.portal"),
+  REACT_FRAGMENT_TYPE = Symbol.for("react.fragment"),
+  REACT_STRICT_MODE_TYPE = Symbol.for("react.strict_mode"),
+  REACT_PROFILER_TYPE = Symbol.for("react.profiler"),
+  REACT_FORWARD_REF_TYPE = Symbol.for("react.forward_ref"),
+  REACT_SUSPENSE_TYPE = Symbol.for("react.suspense"),
+  REACT_MEMO_TYPE = Symbol.for("react.memo"),
+  REACT_LAZY_TYPE = Symbol.for("react.lazy"),
+  MAYBE_ITERATOR_SYMBOL = Symbol.iterator;
+function getIteratorFn(maybeIterable) {
+  if (null === maybeIterable || "object" !== typeof maybeIterable) return null;
+  maybeIterable =
+    (MAYBE_ITERATOR_SYMBOL && maybeIterable[MAYBE_ITERATOR_SYMBOL]) ||
+    maybeIterable["@@iterator"];
+  return "function" === typeof maybeIterable ? maybeIterable : null;
+}
+function formatProdErrorMessage(code) {
+  for (
+    var url = "https://reactjs.org/docs/error-decoder.html?invariant=" + code,
+      i = 1;
+    i < arguments.length;
+    i++
+  )
+    url += "&args[]=" + encodeURIComponent(arguments[i]);
+  return (
+    "Minified React error #" +
+    code +
+    "; visit " +
+    url +
+    " for the full message or use the non-minified dev environment for full errors and additional helpful warnings."
+  );
+}
+var ReactNoopUpdateQueue = {
+    isMounted: function () {
+      return !1;
+    },
+    enqueueForceUpdate: function () {},
+    enqueueReplaceState: function () {},
+    enqueueSetState: function () {}
+  },
+  emptyObject = {};
+function Component(props, context, updater) {
+  this.props = props;
+  this.context = context;
+  this.refs = emptyObject;
+  this.updater = updater || ReactNoopUpdateQueue;
+}
+Component.prototype.isReactComponent = {};
+Component.prototype.setState = function (partialState, callback) {
+  if (
+    "object" !== typeof partialState &&
+    "function" !== typeof partialState &&
+    null != partialState
+  )
+    throw Error(formatProdErrorMessage(85));
+  this.updater.enqueueSetState(this, partialState, callback, "setState");
+};
+Component.prototype.forceUpdate = function (callback) {
+  this.updater.enqueueForceUpdate(this, callback, "forceUpdate");
+};
+function ComponentDummy() {}
+ComponentDummy.prototype = Component.prototype;
+function PureComponent(props, context, updater) {
+  this.props = props;
+  this.context = context;
+  this.refs = emptyObject;
+  this.updater = updater || ReactNoopUpdateQueue;
+}
+var pureComponentPrototype = (PureComponent.prototype = new ComponentDummy());
+pureComponentPrototype.constructor = PureComponent;
+assign(pureComponentPrototype, Component.prototype);
+pureComponentPrototype.isPureReactComponent = !0;
+var isArrayImpl = Array.isArray,
+  hasOwnProperty = Object.prototype.hasOwnProperty,
+  RESERVED_PROPS$1 = { key: !0, ref: !0, __self: !0, __source: !0 };
+function cloneAndReplaceKey(oldElement, newKey) {
+  return {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type: oldElement.type,
+    key: newKey,
+    ref: oldElement.ref,
+    props: oldElement.props,
+    _owner: oldElement._owner
+  };
+}
+function isValidElement(object) {
+  return (
+    "object" === typeof object &&
+    null !== object &&
+    object.$$typeof === REACT_ELEMENT_TYPE
+  );
+}
+function escape(key) {
+  var escaperLookup = { "=": "=0", ":": "=2" };
+  return (
+    "$" +
+    key.replace(/[=:]/g, function (match) {
+      return escaperLookup[match];
+    })
+  );
+}
+var userProvidedKeyEscapeRegex = /\/+/g;
+function getElementKey(element, index) {
+  return "object" === typeof element && null !== element && null != element.key
+    ? escape("" + element.key)
+    : index.toString(36);
+}
+function mapIntoArray(children, array, escapedPrefix, nameSoFar, callback) {
+  var type = typeof children;
+  if ("undefined" === type || "boolean" === type) children = null;
+  var invokeCallback = !1;
+  if (null === children) invokeCallback = !0;
+  else
+    switch (type) {
+      case "string":
+      case "number":
+        invokeCallback = !0;
+        break;
+      case "object":
+        switch (children.$$typeof) {
+          case REACT_ELEMENT_TYPE:
+          case REACT_PORTAL_TYPE:
+            invokeCallback = !0;
+        }
+    }
+  if (invokeCallback)
+    return (
+      (invokeCallback = children),
+      (callback = callback(invokeCallback)),
+      (children =
+        "" === nameSoFar ? "." + getElementKey(invokeCallback, 0) : nameSoFar),
+      isArrayImpl(callback)
+        ? ((escapedPrefix = ""),
+          null != children &&
+            (escapedPrefix =
+              children.replace(userProvidedKeyEscapeRegex, "$&/") + "/"),
+          mapIntoArray(callback, array, escapedPrefix, "", function (c) {
+            return c;
+          }))
+        : null != callback &&
+          (isValidElement(callback) &&
+            (callback = cloneAndReplaceKey(
+              callback,
+              escapedPrefix +
+                (!callback.key ||
+                (invokeCallback && invokeCallback.key === callback.key)
+                  ? ""
+                  : ("" + callback.key).replace(
+                      userProvidedKeyEscapeRegex,
+                      "$&/"
+                    ) + "/") +
+                children
+            )),
+          array.push(callback)),
+      1
+    );
+  invokeCallback = 0;
+  nameSoFar = "" === nameSoFar ? "." : nameSoFar + ":";
+  if (isArrayImpl(children))
+    for (var i = 0; i < children.length; i++) {
+      type = children[i];
+      var nextName = nameSoFar + getElementKey(type, i);
+      invokeCallback += mapIntoArray(
+        type,
+        array,
+        escapedPrefix,
+        nextName,
+        callback
+      );
+    }
+  else if (
+    ((nextName = getIteratorFn(children)), "function" === typeof nextName)
+  )
+    for (
+      children = nextName.call(children), i = 0;
+      !(type = children.next()).done;
+
+    )
+      (type = type.value),
+        (nextName = nameSoFar + getElementKey(type, i++)),
+        (invokeCallback += mapIntoArray(
+          type,
+          array,
+          escapedPrefix,
+          nextName,
+          callback
+        ));
+  else if ("object" === type)
+    throw (
+      ((array = String(children)),
+      Error(
+        formatProdErrorMessage(
+          31,
+          "[object Object]" === array
+            ? "object with keys {" + Object.keys(children).join(", ") + "}"
+            : array
+        )
+      ))
+    );
+  return invokeCallback;
+}
+function mapChildren(children, func, context) {
+  if (null == children) return children;
+  var result = [],
+    count = 0;
+  mapIntoArray(children, result, "", "", function (child) {
+    return func.call(context, child, count++);
+  });
+  return result;
+}
+function lazyInitializer(payload) {
+  if (-1 === payload._status) {
+    var ctor = payload._result;
+    ctor = ctor();
+    ctor.then(
+      function (moduleObject) {
+        if (0 === payload._status || -1 === payload._status)
+          (payload._status = 1), (payload._result = moduleObject);
+      },
+      function (error) {
+        if (0 === payload._status || -1 === payload._status)
+          (payload._status = 2), (payload._result = error);
+      }
+    );
+    -1 === payload._status && ((payload._status = 0), (payload._result = ctor));
+  }
+  if (1 === payload._status) return payload._result.default;
+  throw payload._result;
+}
+function createCacheRoot() {
+  return new WeakMap();
+}
+function createCacheNode() {
+  return { s: 0, v: void 0, o: null, p: null };
+}
+var ReactCurrentBatchConfig = { transition: null },
+  ReactCurrentOwner = ReactSharedInternals.ReactCurrentOwner,
+  RESERVED_PROPS = { key: !0, ref: !0, __self: !0, __source: !0 };
+function jsx$1(type, config, maybeKey) {
+  var propName,
+    props = {},
+    key = null,
+    ref = null;
+  void 0 !== maybeKey && (key = "" + maybeKey);
+  void 0 !== config.key && (key = "" + config.key);
+  void 0 !== config.ref && (ref = config.ref);
+  for (propName in config)
+    hasOwnProperty.call(config, propName) &&
+      !RESERVED_PROPS.hasOwnProperty(propName) &&
+      (props[propName] = config[propName]);
+  if (type && type.defaultProps)
+    for (propName in ((config = type.defaultProps), config))
+      void 0 === props[propName] && (props[propName] = config[propName]);
+  return {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type: type,
+    key: key,
+    ref: ref,
+    props: props,
+    _owner: ReactCurrentOwner.current
+  };
+}
+exports.Children = {
+  map: mapChildren,
+  forEach: function (children, forEachFunc, forEachContext) {
+    mapChildren(
+      children,
+      function () {
+        forEachFunc.apply(this, arguments);
+      },
+      forEachContext
+    );
+  },
+  count: function (children) {
+    var n = 0;
+    mapChildren(children, function () {
+      n++;
+    });
+    return n;
+  },
+  toArray: function (children) {
+    return (
+      mapChildren(children, function (child) {
+        return child;
+      }) || []
+    );
+  },
+  only: function (children) {
+    if (!isValidElement(children)) throw Error(formatProdErrorMessage(143));
+    return children;
+  }
+};
+exports.Fragment = REACT_FRAGMENT_TYPE;
+exports.Profiler = REACT_PROFILER_TYPE;
+exports.StrictMode = REACT_STRICT_MODE_TYPE;
+exports.Suspense = REACT_SUSPENSE_TYPE;
+exports.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED =
+  ReactSharedInternals;
+exports.__SECRET_SERVER_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED =
+  ReactServerSharedInternals;
+exports.cache = function (fn) {
+  return function () {
+    var dispatcher = ReactCurrentCache.current;
+    if (!dispatcher) return fn.apply(null, arguments);
+    var fnMap = dispatcher.getCacheForType(createCacheRoot);
+    dispatcher = fnMap.get(fn);
+    void 0 === dispatcher &&
+      ((dispatcher = createCacheNode()), fnMap.set(fn, dispatcher));
+    fnMap = 0;
+    for (var l = arguments.length; fnMap < l; fnMap++) {
+      var arg = arguments[fnMap];
+      if (
+        "function" === typeof arg ||
+        ("object" === typeof arg && null !== arg)
+      ) {
+        var objectCache = dispatcher.o;
+        null === objectCache && (dispatcher.o = objectCache = new WeakMap());
+        dispatcher = objectCache.get(arg);
+        void 0 === dispatcher &&
+          ((dispatcher = createCacheNode()), objectCache.set(arg, dispatcher));
+      } else
+        (objectCache = dispatcher.p),
+          null === objectCache && (dispatcher.p = objectCache = new Map()),
+          (dispatcher = objectCache.get(arg)),
+          void 0 === dispatcher &&
+            ((dispatcher = createCacheNode()),
+            objectCache.set(arg, dispatcher));
+    }
+    if (1 === dispatcher.s) return dispatcher.v;
+    if (2 === dispatcher.s) throw dispatcher.v;
+    try {
+      var result = fn.apply(null, arguments);
+      fnMap = dispatcher;
+      fnMap.s = 1;
+      return (fnMap.v = result);
+    } catch (error) {
+      throw ((result = dispatcher), (result.s = 2), (result.v = error), error);
+    }
+  };
+};
+exports.cloneElement = function (element, config, children) {
+  if (null === element || void 0 === element)
+    throw Error(formatProdErrorMessage(267, element));
+  var props = assign({}, element.props),
+    key = element.key,
+    ref = element.ref,
+    owner = element._owner;
+  if (null != config) {
+    void 0 !== config.ref &&
+      ((ref = config.ref), (owner = ReactCurrentOwner$1.current));
+    void 0 !== config.key && (key = "" + config.key);
+    if (element.type && element.type.defaultProps)
+      var defaultProps = element.type.defaultProps;
+    for (propName in config)
+      hasOwnProperty.call(config, propName) &&
+        !RESERVED_PROPS$1.hasOwnProperty(propName) &&
+        (props[propName] =
+          void 0 === config[propName] && void 0 !== defaultProps
+            ? defaultProps[propName]
+            : config[propName]);
+  }
+  var propName = arguments.length - 2;
+  if (1 === propName) props.children = children;
+  else if (1 < propName) {
+    defaultProps = Array(propName);
+    for (var i = 0; i < propName; i++) defaultProps[i] = arguments[i + 2];
+    props.children = defaultProps;
+  }
+  return {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type: element.type,
+    key: key,
+    ref: ref,
+    props: props,
+    _owner: owner
+  };
+};
+exports.createElement = function (type, config, children) {
+  var propName,
+    props = {},
+    key = null,
+    ref = null;
+  if (null != config)
+    for (propName in (void 0 !== config.ref && (ref = config.ref),
+    void 0 !== config.key && (key = "" + config.key),
+    config))
+      hasOwnProperty.call(config, propName) &&
+        !RESERVED_PROPS$1.hasOwnProperty(propName) &&
+        (props[propName] = config[propName]);
+  var childrenLength = arguments.length - 2;
+  if (1 === childrenLength) props.children = children;
+  else if (1 < childrenLength) {
+    for (var childArray = Array(childrenLength), i = 0; i < childrenLength; i++)
+      childArray[i] = arguments[i + 2];
+    props.children = childArray;
+  }
+  if (type && type.defaultProps)
+    for (propName in ((childrenLength = type.defaultProps), childrenLength))
+      void 0 === props[propName] &&
+        (props[propName] = childrenLength[propName]);
+  return {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type: type,
+    key: key,
+    ref: ref,
+    props: props,
+    _owner: ReactCurrentOwner$1.current
+  };
+};
+exports.createRef = function () {
+  return { current: null };
+};
+exports.createServerContext = function () {
+  throw Error(formatProdErrorMessage(248));
+};
+exports.forwardRef = function (render) {
+  return { $$typeof: REACT_FORWARD_REF_TYPE, render: render };
+};
+exports.isValidElement = isValidElement;
+exports.jsx = jsx$1;
+exports.jsxDEV = void 0;
+exports.jsxs = jsx$1;
+exports.lazy = function (ctor) {
+  return {
+    $$typeof: REACT_LAZY_TYPE,
+    _payload: { _status: -1, _result: ctor },
+    _init: lazyInitializer
+  };
+};
+exports.memo = function (type, compare) {
+  return {
+    $$typeof: REACT_MEMO_TYPE,
+    type: type,
+    compare: void 0 === compare ? null : compare
+  };
+};
+exports.startTransition = function (scope, options) {
+  var prevTransition = ReactCurrentBatchConfig.transition;
+  ReactCurrentBatchConfig.transition = {};
+  enableTransitionTracing &&
+    void 0 !== options &&
+    void 0 !== options.name &&
+    ((ReactCurrentBatchConfig.transition.name = options.name),
+    (ReactCurrentBatchConfig.transition.startTime = -1));
+  try {
+    scope();
+  } finally {
+    ReactCurrentBatchConfig.transition = prevTransition;
+  }
+};
+exports.use = function (usable) {
+  return ReactCurrentDispatcher.current.use(usable);
+};
+exports.useCallback = function (callback, deps) {
+  return ReactCurrentDispatcher.current.useCallback(callback, deps);
+};
+exports.useContext = function (Context) {
+  return ReactCurrentDispatcher.current.useContext(Context);
+};
+exports.useDebugValue = function () {};
+exports.useId = function () {
+  return ReactCurrentDispatcher.current.useId();
+};
+exports.useMemo = function (create, deps) {
+  return ReactCurrentDispatcher.current.useMemo(create, deps);
+};
+exports.version = "18.3.0-www-modern-3ccafb81";
